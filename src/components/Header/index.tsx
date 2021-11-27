1a9fd1d2a16f6ac153b0fd3fa3048f9f6a5471ea
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-11-27 01:46:13 -0300

    Created header component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+interface HeaderProps {
+  className: string;
+}
+
+export default function Header({ className }: HeaderProps): JSX.Element {
+  return (
+    <header className={`${styles.container} ${className}`}>
+      <Link href="/">
+        <a>
+          <img src="/images/spacetraveling.png" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
